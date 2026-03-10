91190ba355fae06f2e74fb940b8a67e7c61fd985
VandanS-130704 <vandanvs13@gmail.com>
2026-03-10 20:20:51 +0530

    Update API invokeUrl in config.js

--- js/config.js
+++ js/config.js
@@ -5,7 +5,8 @@ window._config = {
         region: 'us-west-2'
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://k6mqvqr59b.execute-api.us-west-2.amazonaws.com/dev',
     }
 };
+
 
